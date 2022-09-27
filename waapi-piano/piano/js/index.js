1278df94aa5df8e12e89ecaf42a20371c22b6756
ming <ming@cccc>
2022-09-28 00:13:22 +0800

    1. 補上FFT 2. 音源使用52號音，並且改為通過計算獲得其他的音源

--- waapi-piano/piano/js/index.js
+++ waapi-piano/piano/js/index.js
@@ -1,46 +1,35 @@
-(function() {
+(async () => {
     'use strict';
 
-    var isSP, ctx, xml, data14, data, frequencyRatioTempered, keyboards;
+    var isSP, ctx, frequencyRatioTempered, keyboards;
 
     // コンテキストを生成
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     ctx = new AudioContext();
 
+    const analyser = ctx.createAnalyser()
+    analyser.fftSize = fftSize
+
     // 音源ファイルをバイナリデータとして取得
-    xml = new XMLHttpRequest();
-    xml.responseType = 'arraybuffer';
-    xml.open('GET', 'waapi-piano/piano/media/German Concert D 034 083.ogg', true); //這是
-    xml.onload = function() {
-        // 音源ファイルをバイナリデータからデコード
-        ctx.decodeAudioData(
-            xml.response,
-            function(_data) {
-                data14 = _data;
-            },
-            function(e) {
-                alert(e.err);
-            }
-        );
-    };
-    xml.send();
-    
-    const xml2 = new XMLHttpRequest();
-    xml2.responseType = 'arraybuffer';
-    xml2.open('GET', 'waapi-piano/piano/media/German Concert D 072 083.ogg', true); //這是C5，第52號鍵
-    xml2.onload = function() {
-        // 音源ファイルをバイナリデータからデコード
-        ctx.decodeAudioData(
-            xml2.response,
-            function(_data) {
-                data = _data;
-            },
-            function(e) {
-                alert(e.err);
-            }
-        );
-    };
-    xml2.send();
+    const data = await new Promise((resolve, reject) => {
+        const xml = new XMLHttpRequest();
+        xml.responseType = 'arraybuffer';
+        xml.open('GET', 'waapi-piano/piano/media/German Concert D 072 083.ogg', true); //這是
+        // xml.open('GET', 'waapi-piano/piano/media/piano.wav', true); //這是
+        xml.onload = function () {
+            // 音源ファイルをバイナリデータからデコード
+            ctx.decodeAudioData(
+                xml.response,
+                function (_data) {
+                    resolve(_data)
+                },
+                function (e) {
+                    alert(e.err);
+                }
+            );
+        };
+        xml.send();
+    })
 
     isSP = typeof window.ontouchstart !== 'undefined';
 
@@ -51,96 +40,95 @@
     keyboards = Array.prototype.slice.call(
         document.getElementsByClassName('keyboard')
     );
-    // 高い方から順に、鍵盤要素のクリック/タッチイベントを登録する
-    // keyboards.slice(0,13).reverse().map(function(keyboard, index) {
-    //     var i, frequencyRatio;
-    //     // 基準音から何音はなれているかで、周波数比を求める
-    //     frequencyRatio = 1;
-    //     for (i = 0; i < index; i++) {
-    //         frequencyRatio /= frequencyRatioTempered;
-    //     }
-    //     keyboard.addEventListener(isSP ? 'touchstart' : 'click', function() {
-    //         var bufferSource;
-    //         bufferSource = ctx.createBufferSource();
-    //         bufferSource.buffer = data14;
-    //         // 音源再生速度の比率変更で、音源の高さを調整
-    //         bufferSource.playbackRate.value = frequencyRatio;
-    //         bufferSource.connect(ctx.destination);
-    //         bufferSource.start(0);
-    //     });
-    // });
-    // keyboards.slice(13,52).reverse().map(function(keyboard, index) {
-    //     var i, frequencyRatio;
-    //     // 基準音から何音はなれているかで、周波数比を求める
-    //     frequencyRatio = 1;
-    //     for (i = 0; i < index; i++) {
-    //         frequencyRatio /= frequencyRatioTempered;
-    //     }
-    //     keyboard.addEventListener(isSP ? 'touchstart' : 'click', function() {
-    //         var bufferSource;
-    //         bufferSource = ctx.createBufferSource();
-    //         bufferSource.buffer = data;
-    //         // 音源再生速度の比率変更で、音源の高さを調整
-    //         bufferSource.playbackRate.value = frequencyRatio;
-    //         bufferSource.connect(ctx.destination);
-    //         bufferSource.start(0);
-    //     });
-    // });
-    // keyboards.slice(52).map(function(keyboard, index) {
-    //     var i, frequencyRatio;
-    //     // 基準音から何音はなれているかで、周波数比を求める
-    //     frequencyRatio = 1;
-    //     for (i = 0; i < index; i++) {
-    //         frequencyRatio *= frequencyRatioTempered;
-    //     }
-    //     keyboard.addEventListener(isSP ? 'touchstart' : 'click', function() {
-    //         var bufferSource;
-    //         bufferSource = ctx.createBufferSource();
-    //         bufferSource.buffer = data;
-    //         // 音源再生速度の比率変更で、音源の高さを調整
-    //         bufferSource.playbackRate.value = frequencyRatio;
-    //         bufferSource.connect(ctx.destination);
-    //         bufferSource.start(0);
-    //     });
-    // });
 
-    keyboards.map(async (keyboard, index) => {
-        console.log(index)
-        const data = await new Promise((resolve, reject) => {
-            const xml = new XMLHttpRequest();
-            xml.responseType = 'arraybuffer';
-            xml.open('GET', `waapi-piano/piano/media/German Concert D ${(index-0+21).toString().padStart(3, '0')} 083.ogg`, true); //這是C5，第52號鍵
-            xml.onload = function() {
-                // 音源ファイルをバイナリデータからデコード
-                ctx.decodeAudioData(
-                    xml.response,
-                    function(_data) {
-                        resolve(_data)
-                    },
-                    function(e) {
-                        alert(e.err);
-                    }
-                );
-            };
-            xml.send();
-        })
-
-        keyboard.addEventListener(isSP ? 'touchstart' : 'click', function() {
+    //方法1
+    keyboards.slice(0).map(function (keyboard, index) {
+        var i, frequencyRatio;
+        // 基準音から何音はなれているかで、周波数比を求める
+        frequencyRatio = (Math.pow(2, 1 / 12) ** (52 - 49)) * 440;
+        frequencyRatio = (Math.pow(2, 1 / 12) ** (index + 1 - 49)) * 440 / frequencyRatio;
+        keyboard.addEventListener(isSP ? 'touchstart' : 'click', function () {
             var bufferSource;
             bufferSource = ctx.createBufferSource();
             bufferSource.buffer = data;
             // 音源再生速度の比率変更で、音源の高さを調整
-            bufferSource.playbackRate.value = 1;
+            bufferSource.playbackRate.value = frequencyRatio;
             bufferSource.connect(ctx.destination);
+            
+            bufferSource.connect(analyser)
             bufferSource.start(0);
         });
     });
 
+    //方法2
+    // keyboards.map(async (keyboard, index) => {
+    //     console.log(index)
+    //     const data = await new Promise((resolve, reject) => {
+    //         const xml = new XMLHttpRequest();
+    //         xml.responseType = 'arraybuffer';
+    //         xml.open('GET', `waapi-piano/piano/media/German Concert D ${(index-0+21).toString().padStart(3, '0')} 083.ogg`, true); //這是C5，第52號鍵
+    //         xml.onload = function() {
+    //             // 音源ファイルをバイナリデータからデコード
+    //             ctx.decodeAudioData(
+    //                 xml.response,
+    //                 function(_data) {
+    //                     resolve(_data)
+    //                 },
+    //                 function(e) {
+    //                     alert(e.err);
+    //                 }
+    //             );
+    //         };
+    //         xml.send();
+    //     })
+    //     keyboard.addEventListener(isSP ? 'touchstart' : 'click', function() {
+    //         var bufferSource;
+    //         bufferSource = ctx.createBufferSource();
+    //         bufferSource.buffer = data;
+    //         // 音源再生速度の比率変更で、音源の高さを調整
+    //         bufferSource.playbackRate.value = 1;
+    //         bufferSource.connect(ctx.destination);
+    //         bufferSource.connect(analyser)
+    //         bufferSource.start(0);
+    //     });
+    // });
+
     let keyboard_black_number = 0
-    document.querySelectorAll('.keyboard').forEach((v,k)=>{
+    document.querySelectorAll('.keyboard').forEach((v, k) => {
         if (v.classList.contains('keyboard-black')) {
             v.style.left = `${100/52*(k- keyboard_black_number)-(100/52/4)}%`
             keyboard_black_number++
         }
     })
+
+    
+    //讀取FFT Data並顯示
+    const getFFTData = () => {
+      const fftData = new Uint8Array(fftSize/2)
+      const fftData_Time = new Uint8Array(fftSize/2)
+
+        const fftArray = new Uint8Array(fftSize/2)
+        analyser.getByteFrequencyData(fftData)
+        
+        // fftArray.forEach((v2,k2) => fftData[k2]=fftData[k2] > v2 ? fftData[k2] : v2)
+        
+        const fftArray_Time = new Uint8Array(fftSize/2)
+        analyser.getByteTimeDomainData(fftData_Time)
+        // fftArray_Time.forEach((v2,k2) => fftData_Time[k2]=fftData_Time[k2] > v2 ? fftData_Time[k2] : v2)
+      
+      
+      // console.log(fftData.map(v=>v*44100/2/(fftSize/2)))
+      for (const k in fftData) {
+        // if (k>=fftSize/2) break
+        document.querySelector(`#fftData>div:nth-child(${k-0+1})`).style.height = `${fftData[k]}px`
+      }
+      for (const k in fftData_Time) {
+        // if (k>=fftSize/2) break
+        document.querySelector(`#fftData_Time>div:nth-child(${k-0+1})`).style.height = `${fftData_Time[k]}px`
+      }
+    //   if (isPlaying) {
+        setTimeout(getFFTData,15)
+    //   }
+    }
+    getFFTData()
 })();
